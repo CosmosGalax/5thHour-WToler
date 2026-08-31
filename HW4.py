fc0030de6b9053c6551730ed0040d181eb647c72
Wyatt Toler 5th Hour <wt130273@student.wpsok.org>
2026-08-31 12:48:56 -0500

    Submitted HW4

---
+++ HW4.py
@@ -0,0 +1,28 @@
+#Name:Wyatt Toler
+#Class: 5th Hour
+#Assignment: HW4
+
+#1. Print "Hello World!"
+print("Hello World")
+#2. import the 'math' library
+import math
+#3. Create two variables, x and y, that asks the user for a decimal (float) for x and an integer for y.
+x = float(input("Enter Number"))
+y = float(input("Enter Number"))
+#4. Create a variable with the value that is x and y added together.
+xy = x + y
+#5. Print the variable from #4.
+print(xy)
+#6. Create a variable with the value that is x and y added together, then divide the sum by 3.
+xyx = xy / 3
+#7. Print the variable from #6.
+print(xyx)
+#8. Create a variable with the value of the square root of y, then print the result.
+VarYsquare = (math.sqrt(y))
+print(VarYsquare)
+#9. Use the round function to round x to the nearest tenths place (EX: 1.17 rounds to 1.1). Print the result.
+print(round(x, 1))
+#10. Use the ceiling function to round x up to the nearest whole number. Print the result.
+print(math.ceil(x))
+#11. Use the floor function to round x down to the nearest whole number. Print the result.
+print(math.floor(x))
