ea49439cd86cc382a2b03bf2f25ad9b19ea152bd
Wyatt Toler 5th Hour <wt130273@student.wpsok.org>
2026-09-15 12:49:40 -0500

    HW7 PUSHED

---
+++ HW7.py
@@ -0,0 +1,9 @@
+#Name:Wyatt Toler
+#Class: 5th Hour
+#Assignment: HW7
+
+
+#12. Print ("\u2764 \u0059 \u004F \u0055 \u0020 \u0044 \u0049 \u0044 \u0020 \u0049 \u0054 \u2764") and ignore steps 1 through 11
+print("\u2764 \u0059 \u004F \u0055 \u0020 \u0044 \u0049 \u0044 \u0020 \u0049 \u0054 \u2764")
+
+# I hate you omg💔💔
