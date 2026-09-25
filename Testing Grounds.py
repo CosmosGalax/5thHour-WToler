64a46c87ff2838eeda25fa714e6d49b9f0287486
Wyatt Toler 5th Hour <wt130273@student.wpsok.org>
2026-09-25 13:18:40 -0500

    Testing Grounds Pushed

--- Testing Grounds.py
+++ Testing Grounds.py
@@ -1,26 +1,20 @@
-Var = "I"
-BackUpVar = "I"
-C = 0
-for _ in range(100):
-    C = C + 1
-    if C == 20:
-        Var = "IIII"
-    if C == 40:
-        Var = "IIII"
-
-    if C == 60:
-        Var = "IIII"
-
-    if C == 80:
-        Var = "IIII"
-
-    if C == 100:
-        Var = "IIII"
-        Var = BackUpVar
-    if C % 2:
-        Var = BackUpVar
-        BackUpVar = BackUpVar + "II"
-        Var = Var + "II"
-        print(Var)
-    elif True:
-        print(Var)
+import random
+AmountOfJackpots = 0
+CountTillJackpot = 2
+JackpotTries = []
+Tries = int(input("How many Tries would you like?"))
+for _ in range(Tries) :
+    Slot1 = random.randint(1, 9)
+    Slot2 = random.randint(1, 9)
+    Slot3 = random.randint(1, 9)
+    SlotList = [Slot1, Slot2, Slot3]
+    print(SlotList)
+    CountTillJackpot = CountTillJackpot + 1
+    if SlotList[0] and SlotList[1] and SlotList[2] == 7:
+        AmountOfJackpots += 1
+        print(SlotList)
+        print("Found Jackpot in", str(CountTillJackpot), "tries")
+        JackpotTries.append(CountTillJackpot)
+        CountTillJackpot = 2
+print("Total about of Jackpots for", Tries, "Tries is", AmountOfJackpots, "or", str(round(Tries/AmountOfJackpots, 2))+ "% chance")
+JackpotTries = []
